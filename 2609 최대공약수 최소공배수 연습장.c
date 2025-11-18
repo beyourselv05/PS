3c712ed57c13082ff544906e3af22ea5fbba33e3
DH kim <beyourselv05@gmail.com>
2025-11-18 22:50:50 +0900

    Update 2609 최대공약수 최소공배수 연습장.c

--- 2609 최대공약수 최소공배수 연습장.c
+++ 2609 최대공약수 최소공배수 연습장.c
@@ -1,14 +1,6 @@
-//x=24, y=18
-//<최대공약수 6>
-//24(1,2,3,4,6,8,12,24)
-//18(1,2,3,6,9,18)
-
-//<최소공배수 72>
-// 24 48 72
-// 18 36 54 72
 #include <stdio.h>
 
-int Euclidean(int a, int b) {
+int Euclidean(int a, int b) {           //최대공약수
     if(b==0) {
         return a;
     }
@@ -17,9 +9,12 @@ int Euclidean(int a, int b) {
     }
 }
 
+int multiple(int a, int b) {            //최소공배수
+    return (a*b)/Euclidean(a, b);
+}
+
 int main() {
     int x, y;
     scanf("%d %d", &x, &y);
-    printf("%d %d", Euclidean(x, y));
-    printf("%d", 18%24);
+    printf("%d\n%d", Euclidean(x, y), multiple(x, y));
 }
