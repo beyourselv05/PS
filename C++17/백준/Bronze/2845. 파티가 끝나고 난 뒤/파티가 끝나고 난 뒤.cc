57139f6bf3231745dde8ea2340018718573ad5c3
DH kim <beyourselv05@gmail.com>
2026-01-12 22:24:02 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    int x, y, z, q, w;
+    cin >> a >> b;
+    cin >> x >> y >> z >> q >> w;
+    cout << x-a*b << " " << y-a*b << " " << z-a*b << " " << q-a*b << " " << w-a*b;
+}
