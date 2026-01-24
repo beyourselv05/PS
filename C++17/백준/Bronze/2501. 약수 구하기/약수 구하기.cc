c01ac889322696a1cf3eeef213fccd85a300ff85
DH kim <beyourselv05@gmail.com>
2026-01-24 22:30:24 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int N, K;   cin >> N >> K;
+    vector<int> arr;
+    for(int i=1; i<=N; i++) {
+        if(N%i == 0) {
+            arr.push_back(i);
+        }
+    }
+    cout << arr[K-1];
+}
